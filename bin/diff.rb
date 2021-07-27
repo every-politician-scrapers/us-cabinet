df5ff7890b65d8ff1541120e3739c8a2034a9e1c
Tony Bowden <tony@tmtm.com>
2021-07-27 11:03:40 +0300

    update to every-politician-scraper 0.0.12

--- bin/diff.rb
+++ bin/diff.rb
@@ -5,9 +5,9 @@ require 'every_politician_scraper/comparison'
 
 class UnitedStatesComparison < EveryPoliticianScraper::Comparison
   def wikidata_csv_options
-    { converters: [->(v) { v.gsub(/^United States /, '').gsub(' of the United States', '') }] }
+    { converters: [->(v) { v.to_s.gsub(/^United States /, '').gsub(' of the United States', '') }] }
   end
 end
 
 diff = UnitedStatesComparison.new('data/wikidata.csv', 'data/official.csv').diff
-puts diff.sort_by { |r| [r.first, r.last.to_s] }.reverse.map(&:to_csv)
+puts diff.sort_by { |r| [r.first, r[1].to_s] }.reverse.map(&:to_csv)
