685cbac3e22b8ce8b4bf0aedd3c9a89094004cce
Tony Bowden <tony@tmtm.com>
2021-09-08 21:59:31 +0300

    Update to EPS 0.1.2

--- bin/scraper/official.rb
+++ bin/scraper/official.rb
@@ -5,25 +5,17 @@ require 'every_politician_scraper/scraper_data'
 require 'pry'
 
 class MemberList
-  # details for an individual member
-  class Member < Scraped::HTML
-    field :name do
+  class Member
+    def name
       noko.css('h3').text.gsub(/Dr\. /, '').tidy
     end
 
-    field :position do
+    def position
       noko.css('h4').text.tidy
     end
   end
 
-  # The page listing all the members
-  class Members < Scraped::HTML
-    field :members do
-      member_container.map { |member| fragment(member => Member).to_h }
-    end
-
-    private
-
+  class Members
     def member_container
       noko.css('.module__persongrid .persongrid__item')
     end
