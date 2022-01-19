33e8c7647672e9d6ba77f8d3e40d29203d983c05
Tony Bowden <tony@tmtm.com>
2022-01-19 21:22:59 +0200

    compare enwiki WH Chief of Staff

---
+++ sources/wh-cos-enwiki/scraper.rb
@@ -0,0 +1,32 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/scraper_data'
+require 'pry'
+
+class OfficeholderList < OfficeholderListBase
+  decorator RemoveReferences
+  decorator UnspanAllTables
+  decorator WikidataIdsDecorator::Links
+
+  def header_column
+    'Portrait'
+  end
+
+  class Officeholder < OfficeholderBase
+    def columns
+      %w[_ _ name start end].freeze
+    end
+
+    def empty?
+      noko.text.include? 'Vacant'
+    end
+
+    def tds
+      noko.css('td,th')
+    end
+  end
+end
+
+url = ARGV.first
+puts EveryPoliticianScraper::ScraperData.new(url, klass: OfficeholderList).csv
