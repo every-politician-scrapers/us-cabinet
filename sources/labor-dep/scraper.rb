7fc6475105bfe1a61341fc4912d08353cafa8caf
Tony Bowden <tony@tmtm.com>
2022-01-22 09:58:31 +0200

    compare enwiki Deputy Secretary of Labor

---
+++ sources/labor-dep/scraper.rb
@@ -0,0 +1,32 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/scraper_data'
+require 'pry'
+
+class OfficeholderList < OfficeholderListBase
+  decorator RemoveReferences
+  decorator UnspanAllTables
+  decorator WikidataIdsDecorator::Links
+
+  def header_column
+    'Term'
+  end
+
+  class Officeholder < OfficeholderBase
+    def columns
+      %w[num name dates].freeze
+    end
+
+    def empty?
+      tds[1].text == tds[2].text
+    end
+
+    def tds
+      noko.css('td,th')
+    end
+  end
+end
+
+url = ARGV.first
+puts EveryPoliticianScraper::ScraperData.new(url, klass: OfficeholderList).csv
