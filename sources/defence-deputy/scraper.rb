9f0672c4d4c78b47642588ba87ae10ee9ecffc2f
Tony Bowden <tony@tmtm.com>
2022-02-04 14:03:52 +0200

    compare enwiki Deputy Secretary of Defence

---
+++ sources/defence-deputy/scraper.rb
@@ -0,0 +1,33 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/scraper_data'
+require 'pry'
+
+class OfficeholderList < OfficeholderListBase
+  decorator RemoveReferences
+  decorator UnspanAllTables
+  decorator WikidataIdsDecorator::Links
+
+  def header_column
+    'Image'
+  end
+
+  class Officeholder < OfficeholderBase
+    def columns
+      %w[no img name start end].freeze
+    end
+
+    def tds
+      noko.css('td,th')
+    end
+
+    # Complicated dates, so just skip
+    def empty?
+      itemLabel.include?('Stephen Early') || itemLabel.include?('Gordon R. England')
+    end
+  end
+end
+
+url = ARGV.first
+puts EveryPoliticianScraper::ScraperData.new(url, klass: OfficeholderList).csv
